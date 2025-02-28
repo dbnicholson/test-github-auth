b639fa74154b9c3b86f40fd4b22df6975c2f060c
Dan Nicholson <dbn@endlessos.org>
2025-02-28 15:12:25 -0700

    Templating with pug

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log(`Open http://127.0.0.1:3000`)
 })
